32f6ac722215cb81d4f4f81be8fc3ebfbd2a5fd6
Kiyotaka Misoo <misoo.kiyotaka@imsbio.co.jp>
2025-02-26 11:47:09 +0900

    a little modify

--- src/analysis/interface/python_interface/all_run.sh
+++ src/analysis/interface/python_interface/all_run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#python crd_convert.py
-#python trj_analysis_distance.py
-#python trj_analysis_angle.py
-#python trj_analysis_dihedral.py
-#python wham_analysis.py
+python crd_convert.py
+python trj_analysis_distance.py
+python trj_analysis_angle.py
+python trj_analysis_dihedral.py
+python wham_analysis.py
 python mbar_analysis_umbrella_1d.py
 python mbar_analysis_umbrella_block.py
-#python avecrd_analysis.py
-#python kmeans_clustering.py
-#python rmsd_analysis.py
-#python drms_analysis.py
-#python rg_analysis.py
-#python diffusion_analysis.py
-#python msd_analysis.py
-#python hb_analysis_count_atom.py
-#python hb_analysis_count_snap.py
+python avecrd_analysis.py
+python kmeans_clustering.py
+python hb_analysis_count_atom.py
+python hb_analysis_count_snap.py
+python rmsd_analysis.py
+python drms_analysis.py
+python rg_analysis.py
+python msd_analysis.py
+python diffusion_analysis.py
